8f374f1df6d941d86dc7c7a58d5d68910dab399d
wuxmax <github@max-reinhard.de>
2023-07-10 21:58:53 +0200

    updated plotting

--- code/graphbrain_semsim/eval_tools/plots/plot_n_results_vs_thresholds.py
+++ code/graphbrain_semsim/eval_tools/plots/plot_n_results_vs_thresholds.py
@@ -6,7 +6,7 @@ from matplotlib.axes import Axes
 import scienceplots  # noqa
 
 from graphbrain_semsim import logger, PLOT_DIR
-from graphbrain_semsim.conflicts_case_study.models import EvaluationRun, EvaluationScenario
+from graphbrain_semsim.conflicts_case_study.models import EvaluationRun, EvaluationScenario, RefEdgesConfig
 from graphbrain_semsim.conflicts_case_study.scenario_configs import CASE_STUDY, EVAL_SCENARIOS
 from graphbrain_semsim.eval_tools.utils import get_eval_scenario, get_eval_runs, get_variable_threshold_sub_pattern
 
@@ -15,6 +15,7 @@ plt.style.use(['science', 'grid'])
 
 # Increase the font size
 plt.rcParams.update({
+    'text.usetex': False,
     'font.size': 14,            # Set the global font size
     'xtick.labelsize': 14,      # Set the font size for the x-axis tick labels
     'ytick.labelsize': 14,      # Set the font size for the y-axis tick labels
@@ -55,10 +56,12 @@ def plot_n_results_vs_threshold(
 
         # multiple runs for different thresholds
         plot_n_results_vs_variable_threshold(
-            axes, scenario.id, variable_threshold_sub_pattern, x_lim_data, y_lim_data, x_lim_view, y_lim_view
+            axes, scenario.id, variable_threshold_sub_pattern, scenario.ref_edges_configs,
+            x_lim_data, y_lim_data, x_lim_view, y_lim_view
         )
 
-    figure.legend(loc='upper right')
+    # figure.legend(loc='upper right')
+    figure.legend(loc='center left')
     figure.set_dpi(300)
 
     plot_file_path: Path = PLOT_DIR / f"{case_study}_{plot_name}.png"
@@ -71,6 +74,7 @@ def plot_n_results_vs_variable_threshold(
         axes: Axes,
         scenario_id: str,
         variable_threshold_sub_pattern: str,
+        ref_edges_configs: list[RefEdgesConfig],
         x_lim_data: tuple[float | None, float | None] = None,
         y_lim_data: tuple[float | None, float | None] = None,
         x_lim_view: tuple[float | None, float | None] = None,
@@ -79,6 +83,60 @@ def plot_n_results_vs_variable_threshold(
 ):
     eval_runs: list[EvaluationRun] = get_eval_runs(scenario_id)
 
+    if ref_edges_configs:
+        eval_runs_by_ref_edges_source_scenario: dict[str, list[EvaluationRun]] = {
+            ref_edges_config.source_scenario: [] for ref_edges_config in ref_edges_configs
+        }
+        for eval_run in eval_runs:
+            eval_runs_by_ref_edges_source_scenario[eval_run.ref_edges_config.source_scenario].append(eval_run)
+
+        eval_runs_: dict[str, list[EvaluationRun]] = {
+            f"{scenario_id}_{ref_edges_config.source_scenario}":
+                eval_runs_by_ref_edges_source_scenario[ref_edges_config.source_scenario]
+            for ref_edges_config in ref_edges_configs
+        }
+
+    else:
+        eval_runs_ = {scenario_id: eval_runs}
+
+    for scenario_label, eval_runs in eval_runs_.items():
+        get_and_plot_data_points(
+            axes,
+            eval_runs,
+            scenario_label,
+            variable_threshold_sub_pattern,
+            x_lim_data, y_lim_data, x_lim_view, y_lim_view
+        )
+
+
+def get_and_plot_data_points(
+        axes: Axes,
+        eval_runs: list[EvaluationRun],
+        scenario_label: str,
+        variable_threshold_sub_pattern: str,
+        x_lim_data: tuple[float | None, float | None] = None,
+        y_lim_data: tuple[float | None, float | None] = None,
+        x_lim_view: tuple[float | None, float | None] = None,
+        y_lim_view: tuple[float | None, float | None] = None
+
+):
+    data_points: list[tuple[float, int]] = construct_and_filter_data_points(
+        axes, eval_runs, variable_threshold_sub_pattern, x_lim_data, y_lim_data, x_lim_view, y_lim_view
+    )
+
+    plot_data = {'threshold': [p[0] for p in data_points], 'n_results': [p[1] for p in data_points]}
+    axes.plot('threshold', 'n_results', data=plot_data, label=scenario_label, marker='o', linestyle='-')
+
+
+def construct_and_filter_data_points(
+        axes: Axes,
+        eval_runs: list[EvaluationRun],
+        variable_threshold_sub_pattern: str,
+        x_lim_data: tuple[float | None, float | None] = None,
+        y_lim_data: tuple[float | None, float | None] = None,
+        x_lim_view: tuple[float | None, float | None] = None,
+        y_lim_view: tuple[float | None, float | None] = None
+) -> list[tuple[float, int]]:
     data_points: list[tuple[float, int]] = list(sorted([
         (eval_run.sub_pattern_configs[variable_threshold_sub_pattern].threshold, len(eval_run.matches))
         for eval_run in eval_runs
@@ -98,8 +156,7 @@ def plot_n_results_vs_variable_threshold(
     if y_lim_view:
         axes.set_ylim(bottom=y_lim_view[0], top=y_lim_view[1])
 
-    plot_data = {'threshold': [p[0] for p in data_points], 'n_results': [p[1] for p in data_points]}
-    axes.plot('threshold', 'n_results', data=plot_data, label=scenario_id, marker='o', linestyle='-')
+    return data_points
 
 
 def plot_n_results_vs_fix_threshold(
@@ -112,22 +169,37 @@ def plot_n_results_vs_fix_threshold(
     axes.axhline(y=len(eval_runs[0].matches), label=scenario_id, linestyle='--')
 
 
+########################
+# ====== Plotting ======
+########################
+
+# plot_n_results_vs_threshold(
+#     plot_name="n_results_vs_thresholds_semsim-fix",
+#     case_study=CASE_STUDY,
+#     scenarios=[
+#         "1_original-pattern",
+#         "2-1_semsim-fix_preds",
+#         "2-2_semsim-fix_preps",
+#         "2-3_semsim-fix_preds_semsim-fix_preps"
+#     ]
+# )
+#
+# plot_n_results_vs_threshold(
+#     plot_name="n_results_vs_thresholds_countries_semsim-fix",
+#     case_study=CASE_STUDY,
+#     scenarios=[
+#         "3-1_any_countries",
+#         "3-2_semsim-fix_countries"
+#     ]
+# )
+
+
 plot_n_results_vs_threshold(
-    plot_name="n_results_vs_thresholds_semsim-fix",
+    plot_name="n_results_vs_thresholds_semsim-ctx_preds",
     case_study=CASE_STUDY,
     scenarios=[
-        "1_original-pattern",
-        "2-1_semsim-fix_preds",
-        "2-2_semsim-fix_preps",
-        "2-3_semsim-fix_preds_semsim-fix_preps"
+        "4-1_semsim-ctx_preds-general",
+        "4-2_semsim-ctx_preds-countries"
     ]
 )
 
-plot_n_results_vs_threshold(
-    plot_name="n_results_vs_thresholds_counties_semsim-fix",
-    case_study=CASE_STUDY,
-    scenarios=[
-        "3-1_any_countries",
-        "3-2_semsim-fix_countries"
-    ]
-)
